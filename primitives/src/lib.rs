52815ebf6de5734f75c7683d605571f46e0a8921
tensor-programming <tensordeveloper@gmail.com>
2020-06-18 00:10:33 -0400

    pub primitives

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -1,6 +1,7 @@
-mod cipher;
-mod hash;
-mod key_derv_func;
-mod pbkdf;
-mod rng;
-mod signing;
+pub mod auth;
+pub mod cipher;
+pub mod hash;
+pub mod key_derv_func;
+pub mod pbkdf;
+pub mod rng;
+pub mod signing;
