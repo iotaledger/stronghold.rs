6e864037a55eb910888cfbb0ef87eaee00ff0026
tensor-programming <tensordeveloper@gmail.com>
2020-06-18 00:10:16 -0400

    add chacha

--- crypto/src/internal/chacha.rs
+++ crypto/src/internal/chacha.rs
@@ -29,3 +29,47 @@ fn chacha20_rounds(state: &mut [u32]) {
         quarter_round!(3, 4, 9, 14);
     }
 }
+
+pub fn h_chacha20_hash(key: &[u8], nonce: &[u8], buf: &mut [u8]) {
+    let mut state = vec![0u32; 16];
+    (0..4).for_each(|i| state[i] = BASIS[i]);
+    (4..12).for_each(|i| state[i] = read32_little_endian!(&key[(i - 4) * 4..]));
+    (12..16).for_each(|i| state[i] = read32_little_endian!(&nonce[(i - 12) * 4..]));
+
+    chacha20_rounds(&mut state);
+
+    let (buf_a, buf_b) = buf.split_at_mut(16);
+    (0..4).for_each(|i| write32_little_endian!(state[i] => &mut buf_a[i* 4..]));
+    (12..16).for_each(|i| write32_little_endian!(state[i] => &mut buf_b[(i - 12) * 4..]));
+}
+
+pub fn chacha20_ietf_block(key: &[u8], nonce: &[u8], n: u32, buf: &mut [u8]) {
+    let mut state = vec![0u32; 32];
+    let (init, mixed) = state.split_at_mut(16);
+
+    (0..4).for_each(|i| init[i] = BASIS[i]);
+    (4..12).for_each(|i| init[i] = read32_little_endian!(&key[(i - 4) * 4..]));
+    init[12] = n;
+    (13..16).for_each(|i| init[i] = read32_little_endian!(&nonce[(i - 13) * 4..]));
+
+    mixed.copy_from_slice(init);
+    chacha20_rounds(mixed);
+    (0..16).for_each(|i| mixed[i] = add!(mixed[i], init[i]));
+    (0..16).for_each(|i| write32_little_endian!(mixed[i] => &mut buf[i * 4..]));
+}
+
+pub fn chacha20_block(key: &[u8], nonce: &[u8], n: u64, buf: &mut [u8]) {
+    let mut state = vec![0u32; 32];
+    let (init, mixed) = state.split_at_mut(16);
+
+    (0..4).for_each(|i| init[i] = BASIS[i]);
+    (4..12).for_each(|i| init[i] = read32_little_endian!(&key[(i - 4) * 4..]));
+    split64_little_endian!(n => &mut init[12..]);
+    (14..16).for_each(|i| init[i] = read32_little_endian!(&nonce[(i - 14) * 4..]));
+
+    mixed.copy_from_slice(init);
+    chacha20_rounds(mixed);
+
+    (0..16).for_each(|i| mixed[i] = add!(mixed[i], init[i]));
+    (0..16).for_each(|i| write32_little_endian!(mixed[i] => &mut buf[i * 4..]));
+}
