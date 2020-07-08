9464104eb67b125ef633649e4afebcf8c724ed48
Marcelo R. Bianchi <marcelo.bianchi@iota.org>
2020-07-08 17:35:50 -0300

    Adds implementation folder with empty rust project files

---
+++ stronghold/src/lib.rs
@@ -0,0 +1,10 @@
+//! stronghold.rs
+
+#![warn(missing_docs, rust_2018_idioms)]
+#![allow(unused_variables, dead_code)]
+
+/// Simple IOTA Address Module
+mod address;
+
+/// Stronghold Account Module
+mod account;
