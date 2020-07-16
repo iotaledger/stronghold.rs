c346bfd0cfa2ff889dfbe9f0fc1369c11b193ba3
Marcelo R. Bianchi <marcelo.bianchi@iota.org>
2020-07-16 14:29:43 -0300

    Clippy Lint Fix (chachapoly_ietf.rs) https://rust-lang.github.io/rust-clippy/master/index.html#try_err

--- parti/crypto/src/chachapoly_ietf.rs
+++ parti/crypto/src/chachapoly_ietf.rs
@@ -56,7 +56,7 @@ pub fn chachapoly_open(
         ChaCha20Ietf::xor(key, nonce, 1, data);
         Ok(())
     } else {
-        Err(crate::Error::InvalidData)?
+        return Err(crate::Error::InvalidData.into())
     }
 }
 
