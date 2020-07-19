f6d0390595510bf3b7c783041a64b90ef917ffca
tensor-programming <tensordeveloper@gmail.com>
2020-07-18 22:55:04 -0400

    update documentation on vault.

--- parti/vault/src/lib.rs
+++ parti/vault/src/lib.rs
@@ -1,12 +1,21 @@
-// Vault is an in-memory database format which is designed to work
-// without a central server.
-
-// the format of this databse is based off of ordered data chains.
-
-// A Chain of records starts with an `InitTransaction`.  Changes to the data is represented
-// as a descendent of the InitTransaction. The data also features a counter field.
-// Any transaction that isn't a descendent of the InitTransaction is ignored and is not considered valid.
-
+/// Vault is an in-memory database specification which is designed to work
+/// without a central server. The data in the database follows a versioned format
+/// where each user can access a chain of data that documents changes to a group or piece of related data.  Only
+/// the user which holds the associated id and key may modify the data in a chain.
+///
+/// Every Data chain starts with an `InitTransaction`.  The `InitTransaction` contains the user's designated id,
+/// some metadata and no sealed data. Any proceeding data on the same chain needs to be a descendent of this
+/// original record or else it is considered invalid.  
+///
+/// Data can be added to the chain via a `DataTransaction`.  The `DataTransaction` is associated to the chain
+/// through the owner's ID and it also contains its own randomly generated ID.  As with every other record, a
+/// `DataTransaction` also contain a Counter which allows the Vault to identify which record is the latest in the
+/// chain. The counter also helps with determining the order of transactions made to the data in a chain.
+///
+/// Records may also be revoked from the Vault through a `RevocationTransaction`. A `RevocationTransaction` is
+/// created and it references the id of a existing `DataTransaction`. The `RevocationTransaction` stages the
+/// associated record for deletion. The record is deleted when the chain preforms a garbage collection and the
+/// `RevocationTransaction` is deleted along with it.
 use thiserror::Error as DeriveError;
 
 mod base64;
@@ -18,13 +27,10 @@ pub use crate::{
     base64::{Base64Decodable, Base64Encodable},
     crypto_box::{BoxProvider, Decrypt, Encrypt, Key},
     types::utils::{Id, RecordHint},
-    vault::{
-        DBReader, DBView, DBWriter, DeleteRequest, ListResult, ReadRequest, ReadResult, Record,
-        WriteRequest,
-    },
+    vault::{DBReader, DBView, DBWriter, DeleteRequest, ListResult, ReadRequest, ReadResult, Record, WriteRequest},
 };
 
-// Errors for the Vault Crate
+/// Errors for the Vault Crate
 #[derive(DeriveError, Debug)]
 pub enum Error {
     #[error("Database Error: `{0}`")]
