afa0f0b9f726df877a2fb06628e0948968d5d729
tensor-programming <tensordeveloper@gmail.com>
2020-07-30 15:33:55 -0400

    update to bcrypt

--- engine/random/c_src/rng.c
+++ engine/random/c_src/rng.c
@@ -11,13 +11,12 @@
 #include <Security/SecRandom.h>
 #elif defined(USE_CRYPTGENRANDOM)
 #include <windows.h>
-#include <Wincrypt.h>
+#include <Bcrypt.h>
 #elif defined(USE_DEV_RANDOM)
 #include <stdio.h>
 #endif
 
-// Get a secure series of random bytes and pass the length into a buffer.
-// returns 0 upon success and 1 upon error.
+// Get a secure series of random bytes and pass the length into a buffer.returns 0 upon success and 1 upon error.
 uint8_t os_random_secrandom(uint8_t *buf, size_t len)
 {
 #if defined(USE_GETRANDOM)
@@ -28,13 +27,21 @@ uint8_t os_random_secrandom(uint8_t *buf, size_t len)
 #elif defined(USE_SECRANDOM)
     return SecRandomCopyBytes(kSecRandomDefault, len, buf) == errSecSuccess ? 0 : 1;
 #elif defined(USE_CRYPTGENRANDOM)
-    HCRYPTPROV rng;
-    if (CryptAcquireContext(&rng, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT) == 0)
-        return 1;
+    BCRYPT_ALG_HANDLE algo_handle;
+    NTSTATUS ret1 = BCryptOpenAlgorithmProvider(&algo_handle, BCRYPT_RNG_ALGORITHM,
+                                                MS_PRIMITIVE_PROVIDER, 0);
+    if (BCRYPT_SUCCESS(ret1))
+    {
+        NTSTATUS ret2 = BCryptGenRandom(NULL, buf, (ULONG)len, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+        BCryptCloseAlgorithmProvider(algo_handle, 0);
 
-    uint8_t ret_val = CryptGenRandom(rng, (DWORD)len, (BYTE *)buf) == 0 ? 1 : 0;
-    CryptReleaseContext(rng, 0);
-    return ret_val;
+        return BCRYPT_SUCCESS(ret2) ? 0 : 1;
+    }
+    else
+    {
+        return 1;
+    }
+
 #elif defined(USE_DEV_RANDOM)
     FILE *urandom = fopen("/dev/urandom", "r");
     if (urandom == NULL)
