1ff4c352fd2da133130c307419cbb721d0bdd73e
Marcelo R. Bianchi <marcelo.bianchi@iota.org>
2020-08-14 00:30:48 -0300

    Feat: Add dummy_derive

--- stronghold/src/account/dummybip39.rs
+++ stronghold/src/account/dummybip39.rs
@@ -1,6 +1,9 @@
 use unicode_normalization::UnicodeNormalization;
 use pbkdf2;
 use hmac::Hmac;
+use bip39;
+use bitcoin;
+use std::str::FromStr;
 
 use bee_signing_ext::binary::ed25519;
 
@@ -27,4 +30,21 @@ pub(crate) fn dummy_mnemonic_to_ed25_seed(mnemonic: &bip39::Mnemonic, password: 
     let normalized_salt = salt.nfkd().to_string();
     let bytes = _pbkdf2(mnemonic.phrase().as_bytes(), &normalized_salt);
     ed25519::Seed::from_bytes(&bytes).unwrap()
+}
+
+pub(crate) fn dummy_derive(mut seed_to_derivate: bee_signing_ext::binary::ed25519::Seed, path: &str) -> ed25519::PrivateKey {
+    let derivation_path = bitcoin::util::bip32::DerivationPath::from_str(path).expect("Unexpected derivation path");
+    let iterable = derivation_path.into_iter();
+    let iter_len = iterable.len();
+    let mut subseed: ed25519::PrivateKey;
+    for (i,children) in iterable.enumerate() {
+        let index: u64 = u32::from(*children).into();
+        let subseed = ed25519::PrivateKey::generate_from_seed(&seed_to_derivate, index).unwrap();
+        if i+1 == iter_len {
+            return subseed;
+        }
+        let privkey_bytes: &[u8] = subseed.as_bytes();
+        seed_to_derivate = ed25519::Seed::from_bytes(privkey_bytes).unwrap();
+    }
+    panic!("Unexpected derivation path")
 }
