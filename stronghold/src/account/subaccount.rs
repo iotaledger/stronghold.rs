91e8e7414b0548a9e932891455f20bfe12d86eb1
Marcelo R. Bianchi <marcelo.bianchi@iota.org>
2020-08-17 09:13:55 -0300

    Feat: impl SubAccount: fn addresses_increase_counter()

--- stronghold/src/account/subaccount.rs
+++ stronghold/src/account/subaccount.rs
@@ -15,4 +15,14 @@ impl SubAccount {
             change_addresses_counter: 0
         }
     }
+
+    pub fn addresses_increase_counter(&self, internal: bool) -> usize {
+        if internal {
+            self.change_addresses_counter = self.change_addresses_counter + 1;
+            return self.change_addresses_counter;
+        }else{
+            self.receive_addresses_counter = self.receive_addresses_counter + 1;
+            return self.receive_addresses_counter;
+        }
+    }
 }
