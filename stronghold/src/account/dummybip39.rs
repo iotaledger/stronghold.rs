82fea12a970af77cbc790dc47fc8675dbc09f44d
Marcelo R. Bianchi <marcelo.bianchi@iota.org>
2020-08-17 09:03:35 -0300

    Fix: fn dummy_derive_into_address()

--- stronghold/src/account/dummybip39.rs
+++ stronghold/src/account/dummybip39.rs
@@ -50,12 +50,12 @@ pub(crate) fn dummy_derive(mut seed_to_derivate: bee_signing_ext::binary::ed2551
     panic!("Unexpected derivation path")
 }
 
-pub(crate) fn dummy_derive_into_address(ed_priv: ed25519::PrivateKey) {
+pub(crate) fn dummy_derive_into_address(ed_priv: ed25519::PrivateKey) -> String {
     let pubkey = ed_priv.generate_public_key();
     let pubkey_as_bytes_in_box: Box<[u8]> = Box::new(*pubkey.as_bytes());
     let mut pubkey_as_bytes_in_vec: Vec<u8> = pubkey_as_bytes_in_box.into_vec();
     let prefix: Vec<u8> = vec![1];//prefix for ed25 addresses
     let mut data = prefix;
     data.append(&mut pubkey_as_bytes_in_vec);
-    bech32::encode("iota", data.to_base32()).unwrap();
+    bech32::encode("iota", data.to_base32()).unwrap()
 }
