148362af98c2e9e67c141b23a8eed6bb09c5df4e
Marcelo R. Bianchi <marcelo.bianchi@iota.org>
2020-08-30 16:40:13 -0300

    Chore: Adds docs to the highest module level

--- stronghold/src/lib.rs
+++ stronghold/src/lib.rs
@@ -9,7 +9,21 @@
 // an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-//! stronghold.rs
+//! ## Introduction
+//!
+//! IOTA Stronghold is a secure software implementation with the sole purpose of isolating digital secrets from exposure to hackers and accidental leaks. It uses versioned snapshots with double-encryption that can be easily backed up and securely shared between devices. Written in stable rust, it has strong guarantees of memory safety and process integrity. The high-level developer-friendly libraries will integrate the IOTA protocol and serve as a reference implementation for anyone looking for inspiration or best-in-class tooling.
+//!
+//! ## WARNING
+//!
+//! This library has not yet been audited for security, so use at your own peril. Until a formal third-party security audit has taken place, the IOTA Foundation makes no guarantees to the fitness of this library for any purposes.
+//!
+//! As such they are to be seen as experimental and not ready for real-world applications.
+//!
+//! Nevertheless, we are very interested in feedback about the design and implementation, and encourage you to reach out with any concerns or suggestions you may have.
+//!
+//! ## Specification
+//!
+//!
 
 #![warn(missing_docs, rust_2018_idioms)]
 #![allow(unused_variables, dead_code)]
