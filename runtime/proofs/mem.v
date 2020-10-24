1541c2460774968cc2b48bec0f04fe83795cd12c
Gustav Behm <gustav.behm@iota.org>
2020-10-24 15:40:35 +0200

    Sketch a pad and alignment formalization in Coq

---
+++ runtime/proofs/mem.v
@@ -0,0 +1,43 @@
+Require Import PeanoNat.
+
+Definition pad x N :=
+  match x mod N with 0 => 0 | r => N - r end.
+
+Definition aligned x N := pad x N = 0.
+
+Lemma aligned_zero {N}: aligned 0 N.
+Proof.
+  unfold aligned, pad.
+  induction N.
+  - auto.
+  - simpl.
+    rewrite Nat.sub_diag.
+    reflexivity.
+Qed.
+
+Lemma aligned_mod {x N}: aligned x N <-> x mod N = 0.
+Proof.
+  split.
+  + intro H.
+    unfold aligned, pad in H.
+    destruct N.
+    - reflexivity.
+    - assert (P: 0 < S N - x mod S N).
+      {
+        unfold lt.
+        rewrite <- (Nat.sub_diag (x mod S N)), <- Nat.sub_succ_l by auto.
+        refine (Nat.sub_le_mono_r _ _ _ _).
+        refine (proj2 (Nat.mod_bound_pos _ _ (le_0_n _) (le_n_S 0 N (le_0_n _)))).
+      }
+      destruct (x mod S N).
+      ++ reflexivity.
+      ++ rewrite H in P.
+         discriminate (proj1 (Nat.le_0_r 1) P).
+  + intro H.
+    unfold aligned, pad.
+    rewrite H.
+    reflexivity.
+Qed.
+
+Lemma round_left_cancel {N} a b: (aligned a N) -> pad (a + b) N = pad b N.
+Admitted.
