a6278b1fafea88ecb232a4b20eabc668fe89bf29
Gustav Behm <gustav.behm@iota.org>
2020-10-26 12:44:30 +0100

    Only consider pointers aligned if alignment is non-zero

--- runtime/proofs/mem.v
+++ runtime/proofs/mem.v
@@ -6,73 +6,75 @@ Require Import PeanoNat.
 Definition pad x N :=
   match x mod N with 0 => 0 | r => N - r end.
 
-Definition aligned x N := pad x N = 0.
+Definition aligned x N := (N <> 0) /\ pad x N = 0.
 
-Lemma aligned_zero {N}: aligned 0 N.
-Proof.
-  unfold aligned, pad.
-  induction N.
-  - auto.
-  - simpl.
-    rewrite Nat.sub_diag.
-    reflexivity.
-Qed.
-
-Lemma aligned_mod {x N}: aligned x N <-> x mod N = 0.
+Lemma aligned_zero N: aligned 0 (S N).
 Proof.
   split.
-  + intro H.
-    unfold aligned, pad in H.
-    destruct N.
-    - reflexivity.
-    - assert (P: 0 < S N - x mod S N).
-      {
-        unfold lt.
-        rewrite <- (Nat.sub_diag (x mod S N)), <- Nat.sub_succ_l by auto.
-        refine (Nat.sub_le_mono_r _ _ _ _).
-        refine (proj2 (Nat.mod_bound_pos _ _ (le_0_n _) (le_n_S 0 _ (le_0_n _)))).
-      }
-      destruct (x mod S N).
-      ++ reflexivity.
-      ++ rewrite H in P.
-         discriminate (proj1 (Nat.le_0_r 1) P).
-  + intro H.
-    unfold aligned, pad.
-    rewrite H.
-    reflexivity.
+  all: unfold pad; try rewrite Nat.mod_0_l; auto.
 Qed.
 
-Lemma aligned_add_pad x A: A <> 0 -> aligned (x + pad x A) A.
+Lemma unaligned x: aligned x 1.
+Proof.
+  now split.
+Qed.
+
+Lemma aligned_mod {x N}: aligned x N -> x mod N = 0.
+Proof.
+  intros.
+  destruct H as [Nz P].
+  unfold pad in P.
+
+  assert (Q: 0 < N - x mod N). {
+    unfold lt.
+    rewrite <- (Nat.sub_diag (x mod N)), <- Nat.sub_succ_l by auto.
+    refine (Nat.sub_le_mono_r _ _ _ _).
+    refine (proj2 (Nat.mod_bound_pos _ _ (le_0_n _) _)).
+    now apply Nat.neq_0_lt_0.
+  }
+
+  destruct (x mod N). auto.
+  rewrite P in Q.
+  discriminate (proj1 (Nat.le_0_r 1) Q).
+Qed.
+
+Lemma aligned_mod_succ {x N}: aligned x (S N) <-> x mod (S N) = 0.
+Proof.
+  split.
+  + apply aligned_mod.
+  + intro H. unfold aligned.
+    split.
+    - auto.
+    - unfold pad; now rewrite H.
+Qed.
+
+Lemma aligned_add_pad x {A}: A <> 0 -> aligned (x + pad x A) A.
 Proof.
   intro Anz.
-  refine (proj2 aligned_mod _).
-  unfold pad.
-  case (Nat.eq_dec (x mod A) 0).
-  - intro H.
-    rewrite H.
-    rewrite Nat.add_0_r.
-    assumption.
-  - intro H.
-    rewrite <- (Nat.succ_pred _ H).
-    rewrite -> (Nat.succ_pred _ H).
-    rewrite <- (Nat.add_mod_idemp_l _ _ _ Anz).
-    rewrite (Nat.add_sub_assoc _ _ _ (Nat.lt_le_incl _ _ (Nat.mod_upper_bound _ _ Anz))).
-    rewrite Nat.add_comm.
-    rewrite <- (Nat.add_sub_assoc _ _ _ (le_n _)).
-    rewrite Nat.sub_diag.
-    rewrite Nat.add_0_r.
-    apply (Nat.mod_same _ Anz).
+  case_eq A.
+  + intro A0. exfalso. now refine (Anz _).
+  + intros n _.
+    refine (proj2 aligned_mod_succ _).
+    unfold pad.
+    case (Nat.eq_dec (x mod S n) 0).
+    - intro H. now rewrite H, Nat.add_0_r.
+    - intro H.
+      rewrite <- (Nat.succ_pred _ H).
+      rewrite -> (Nat.succ_pred _ H).
+      rewrite <- (Nat.add_mod_idemp_l _ _ _ (Nat.neq_succ_0 _)).
+      rewrite (Nat.add_sub_assoc _ _ _ (Nat.lt_le_incl _ _ (Nat.mod_upper_bound _ _ (Nat.neq_succ_0 _)))).
+      rewrite Nat.add_comm.
+      rewrite <- (Nat.add_sub_assoc _ _ _ (le_n _)).
+      rewrite Nat.sub_diag, Nat.add_0_r.
+      apply (Nat.mod_same _ (Nat.neq_succ_0 _)).
 Qed.
 
-Lemma round_left_cancel {N a} b: aligned a N -> pad (a + b) N = pad b N.
+Lemma round_left_cancel {N a} b: aligned a (S N) -> pad (a + b) (S N) = pad b (S N).
 Proof.
-  intro H.
+  intro A.
   unfold pad.
-  case (Nat.eq_dec N 0).
-  - intro z. rewrite z. auto.
-  - intro nz.
-    rewrite <- (Nat.add_mod_idemp_l a b _ nz), (proj1 aligned_mod H), Nat.add_0_l.
-    reflexivity.
+  rewrite <- (Nat.add_mod_idemp_l a b _ (proj1 A)).
+  now rewrite (aligned_mod A), Nat.add_0_l.
 Qed.
 
 Axiom accessible : nat -> Prop.
@@ -90,24 +92,26 @@ Record Allocation (n A P: nat) := mkAllocation {
   page_alignment_post: aligned (data + n + pad_post) P;
 }.
 
-Lemma naive_allocator {P} (M: mmap P) (Pnz: P <> 0):
-  forall n {A}, A <> 0 -> aligned P A -> Allocation n A P.
+Lemma naive_allocator {P} (M: mmap P):
+  forall n {A}, aligned P A -> Allocation n A P.
 Proof.
-  intros n A Anz.
+  intros n A.
 
   pose (N := P + n).
   destruct (M N) as [x [XP XAcc]].
+  pose (Pnz := proj1 XP).
   pose (k := (pad (x + P) A) / P).
   pose (p0 := (pad (x + P) A) mod P).
   pose (pi i := p0 + i * A).
   pose (di i := x + (1 + k) * P + pi i).
   pose (qi i := pad (di i + n) P).
 
-  intro PA.
+  intros PA.
+  pose (Anz := proj1 PA).
 
   assert (XA: x mod A = 0). {
-    destruct (proj1 (Nat.mod_divides x P Pnz) (proj1 aligned_mod XP)) as [i I].
-    destruct (proj1 (Nat.mod_divides P A Anz) (proj1 aligned_mod PA)) as [j J].
+    destruct (proj1 (Nat.mod_divides x P Pnz) (aligned_mod XP)) as [i I].
+    destruct (proj1 (Nat.mod_divides P A Anz) (aligned_mod PA)) as [j J].
     rewrite I, J.
     rewrite <- Nat.mul_assoc, Nat.mul_comm.
     apply (Nat.mod_mul _ _ Anz).
@@ -116,7 +120,7 @@ Proof.
   assert (XPA: (x + P) mod A = 0). {
     rewrite <- (Nat.add_mod_idemp_l x P A Anz).
     rewrite XA.
-    exact (proj1 aligned_mod PA).
+    exact (aligned_mod PA).
   }
 
   assert (P0: p0 = 0). {
@@ -136,10 +140,9 @@ Proof.
   all: unfold qi, di, pi.
   all: rewrite P0, Nat.add_0_l, Nat.mul_0_l, Nat.add_0_r.
   all: rewrite K0, Nat.add_0_r, Nat.mul_1_l.
-  + refine (proj2 aligned_mod _).
-    rewrite <- (Nat.add_mod_idemp_l _ _ _ Anz).
-    rewrite XA, Nat.add_0_l.
-    now rewrite (proj1 aligned_mod PA).
+  + split.
+    - exact Anz.
+    - unfold pad. now rewrite XPA.
   + intros i I.
     rewrite <- Nat.add_assoc.
     refine (XAcc _ _).
@@ -147,7 +150,7 @@ Proof.
   + rewrite Nat.sub_0_r.
     unfold aligned, pad.
     rewrite <- (Nat.add_mod_idemp_l _ _ _ Pnz).
-    rewrite (proj1 aligned_mod XP), Nat.add_0_l.
-    now rewrite (Nat.mod_same _ Pnz).
-  + apply (aligned_add_pad _ _ Pnz).
+    rewrite (aligned_mod XP), Nat.add_0_l.
+    now rewrite Nat.mod_same.
+  + now apply aligned_add_pad.
 Qed.
