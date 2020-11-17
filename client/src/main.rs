b35bee134d306c9f2ef217ca373007672812cc15
tensor-programming <tensordeveloper@gmail.com>
2020-11-17 14:44:28 -0500

    add error, result and id logic.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,9 @@
+use thiserror::Error as DeriveError;
+
 mod bucket;
 mod cache;
 mod client;
+mod ids;
 mod provider;
 mod secret;
 
@@ -12,6 +15,16 @@ macro_rules! line_error {
     ($str:expr) => {
         concat!($str, " @", file!(), ":", line!())
     };
+}
+
+pub type Result<T> = anyhow::Result<T, Error>;
+
+#[derive(DeriveError, Debug)]
+pub enum Error {
+    #[error("Id Error")]
+    IDError,
+    #[error("Vault Error: {0}")]
+    VaultError(#[from] engine::vault::Error),
 }
 
 fn main() {}
