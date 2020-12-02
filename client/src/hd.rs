3595caa9b298b2d9a4bfbab21901326d394369c8
Gustav Behm <gustav.behm@iota.org>
2020-12-02 13:31:59 +0100

    Correct Segment::from_u32 and clean up test code

--- client/src/hd.rs
+++ client/src/hd.rs
@@ -4,23 +4,15 @@
 // TODO: this module should probably not reside in the client
 
 use crypto::{ed25519::SecretKey, macs::hmac::HMAC_SHA512};
-use num_bigint::BigUint;
+
+// https://github.com/satoshilabs/slips/blob/master/slip-0010.md
+// https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
+// https://en.bitcoin.it/wiki/BIP_0039
 
 #[derive(Debug)]
 pub enum Error {
-    #[allow(dead_code)]
     NotSupported,
     CryptoError(crypto::Error),
-}
-
-// 2^252+27742317777372353535851937790883648493
-// 0x1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ed
-#[allow(dead_code)]
-fn ed25519_group_order() -> BigUint {
-    BigUint::from_bytes_be(&[
-        0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x14, 0xde,
-        0xf9, 0xde, 0xa2, 0xf7, 0x9c, 0xd6, 0x58, 0x12, 0x63, 0x1a, 0x5c, 0xf5, 0xd3, 0xed,
-    ])
 }
 
 pub struct Seed(Vec<u8>);
@@ -35,119 +27,171 @@ impl Seed {
         HMAC_SHA512(&self.0, b"ed25519 seed", &mut I);
         Key(I)
     }
+
+    pub fn derive(&self, chain: &Chain) -> Result<Key, Error> {
+        self.to_master_key().derive(chain)
+    }
 }
 
-#[derive(Debug)]
+type ChainCode = [u8; 32];
+
+#[derive(Copy, Clone)]
 pub struct Key([u8; 64]);
 
 impl Key {
-    fn I_l(&self) -> [u8; 32] {
+    pub fn secret_key(&self) -> Result<SecretKey, Error> {
         let mut I_l = [0; 32];
         I_l.copy_from_slice(&self.0[..32]);
-        I_l
+        // TODO: this conversion should never fail
+        SecretKey::from_le_bytes(I_l).map_err(Error::CryptoError)
     }
 
-    fn I_r(&self) -> [u8; 32] {
+    pub fn chain_code(&self) -> ChainCode {
         let mut I_r = [0; 32];
         I_r.copy_from_slice(&self.0[32..]);
         I_r
     }
 
-    pub fn secret_key(&self) -> Result<SecretKey, Error> {
-        // TODO: this conversion should never fail
-        SecretKey::from_le_bytes(self.I_l()).map_err(Error::CryptoError)
-    }
-
-    pub fn chain_code(&self) -> [u8; 32] {
-        self.I_r()
-    }
-
-    #[allow(dead_code)]
-    fn step(&self, segment: &Segment) -> Result<Key, Error> {
+    pub fn child_key(&self, segment: &Segment) -> Result<Key, Error> {
         if !segment.hardened {
             return Err(Error::NotSupported);
         }
 
         let mut data = [0u8; 1 + 32 + 4];
-        data[1..1 + 32].copy_from_slice(&self.0[..32]);
-        data[1 + 32..1 + 32 + 4].copy_from_slice(&segment.bs);
+        data[1..1 + 32].copy_from_slice(&self.0[..32]); // ser256(k_par) = ser256(parse256(I_l)) = I_l
+        data[1 + 32..1 + 32 + 4].copy_from_slice(&segment.bs); // ser32(i)
 
         let mut I = [0; 64];
         HMAC_SHA512(&data, &self.0[32..], &mut I);
 
         Ok(Self(I))
     }
+
+    pub fn derive(&self, chain: &Chain) -> Result<Key, Error> {
+        let mut k = *self;
+        for c in &chain.0 {
+            k = k.child_key(c)?;
+        }
+        Ok(k)
+    }
 }
 
-#[allow(dead_code)]
-struct Segment {
+pub struct Segment {
     hardened: bool,
     bs: [u8; 4],
 }
 
-#[allow(dead_code)]
 impl Segment {
     pub fn from_u32(i: u32) -> Self {
         Self {
-            hardened: i >= 2147483648,
-            bs: i.to_le_bytes(),
+            hardened: i >= Self::HARDEN_MASK,
+            bs: i.to_be_bytes(), // ser32(i)
         }
     }
 
-    #[allow(dead_code)]
     pub const HARDEN_MASK: u32 = 1 << 31;
 }
 
-#[allow(dead_code)]
-type Chain = Vec<Segment>;
+pub struct Chain(Vec<Segment>);
+
+impl Chain {
+    pub fn empty() -> Self {
+        Self(vec![])
+    }
+
+    pub fn from_u32(is: Vec<u32>) -> Self {
+        Self(is.iter().map(|i| Segment::from_u32(*i)).collect())
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestChain {
+        chain: Chain,
+        chain_code: &'static str,
+        private_key: &'static str,
+    }
+
+    struct TestVector {
+        seed: &'static str,
+        master_chain_code: &'static str,
+        master_private_key: &'static str,
+        chains: Vec<TestChain>,
+    }
+
     #[test]
-    #[ignore = "verify byte-order usages"]
-    // https://github.com/satoshilabs/slips/blob/master/slip-0010.md#test-vector-1-for-ed25519
-    fn ed25519_test_vector_1() -> Result<(), Error> {
-        let seed = Seed::from_bytes(&hex::decode("000102030405060708090a0b0c0d0e0f").unwrap());
+    fn ed25519_test_vectors() -> Result<(), Error> {
+        let tvs = [
+            // https://github.com/satoshilabs/slips/blob/master/slip-0010.md#test-vector-1-for-ed25519
+            TestVector {
+                seed: "000102030405060708090a0b0c0d0e0f",
+                master_chain_code: "90046a93de5380a72b5e45010748567d5ea02bbf6522f979e05c0d8d8ca9fffb",
+                master_private_key: "2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7",
+                chains: vec![
+                    TestChain {
+                        chain: Chain::empty(),
+                        chain_code: "90046a93de5380a72b5e45010748567d5ea02bbf6522f979e05c0d8d8ca9fffb",
+                        private_key: "2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7",
+                    },
+                    TestChain {
+                        chain: Chain::from_u32(vec![Segment::HARDEN_MASK | 0]),
+                        chain_code: "8b59aa11380b624e81507a27fedda59fea6d0b779a778918a2fd3590e16e9c69",
+                        private_key: "68e0fe46dfb67e368c75379acec591dad19df3cde26e63b93a8e704f1dade7a3",
+                    },
+                    TestChain {
+                        chain: Chain::from_u32(vec![Segment::HARDEN_MASK | 0, Segment::HARDEN_MASK | 1]),
+                        chain_code: "a320425f77d1b5c2505a6b1b27382b37368ee640e3557c315416801243552f14",
+                        private_key: "b1d0bad404bf35da785a64ca1ac54b2617211d2777696fbffaf208f746ae84f2",
+                    },
+                    TestChain {
+                        chain: Chain::from_u32(vec![Segment::HARDEN_MASK | 0, Segment::HARDEN_MASK | 1, Segment::HARDEN_MASK | 2]),
+                        chain_code: "2e69929e00b5ab250f49c3fb1c12f252de4fed2c1db88387094a0f8c4c9ccd6c",
+                        private_key: "92a5b23c0b8a99e37d07df3fb9966917f5d06e02ddbd909c7e184371463e9fc9",
+                    },
+                    TestChain {
+                        chain: Chain::from_u32(vec![Segment::HARDEN_MASK | 0, Segment::HARDEN_MASK | 1, Segment::HARDEN_MASK | 2, Segment::HARDEN_MASK | 2]),
+                        chain_code: "8f6d87f93d750e0efccda017d662a1b31a266e4a6f5993b15f5c1f07f74dd5cc",
+                        private_key: "30d1dc7e5fc04c31219ab25a27ae00b50f6fd66622f6e9c913253d6511d1e662",
+                    },
+                    TestChain {
+                        chain: Chain::from_u32(vec![Segment::HARDEN_MASK | 0, Segment::HARDEN_MASK | 1, Segment::HARDEN_MASK | 2, Segment::HARDEN_MASK | 2]),
+                        chain_code: "8f6d87f93d750e0efccda017d662a1b31a266e4a6f5993b15f5c1f07f74dd5cc",
+                        private_key: "30d1dc7e5fc04c31219ab25a27ae00b50f6fd66622f6e9c913253d6511d1e662",
+                    },
+                    TestChain {
+                        chain: Chain::from_u32(vec![Segment::HARDEN_MASK | 0, Segment::HARDEN_MASK | 1, Segment::HARDEN_MASK | 2, Segment::HARDEN_MASK | 2, Segment::HARDEN_MASK | 1000000000]),
+                        chain_code: "68789923a0cac2cd5a29172a475fe9e0fb14cd6adb5ad98a3fa70333e7afa230",
+                        private_key: "8f94d394a8e8fd6b1bc2f3f49f5c47e385281d5c17e65324b0f62483e37e8793",
+                    },
+                ],
+            }
+        ];
 
-        // m
-        let m = seed.to_master_key();
-        let mut expected_master_chain_code = [0u8; 32];
-        hex::decode_to_slice(
-            &"90046a93de5380a72b5e45010748567d5ea02bbf6522f979e05c0d8d8ca9fffb",
-            &mut expected_master_chain_code as &mut [u8],
-        )
-        .unwrap();
-        assert_eq!(expected_master_chain_code, m.chain_code());
+        for tv in &tvs {
+            let seed = Seed::from_bytes(&hex::decode(tv.seed).unwrap());
 
-        let mut expected_master_private_key = [0u8; 32];
-        hex::decode_to_slice(
-            &"2b4be7f19ee27bbf30c667b642d5f4aa69fd169872f8fc3059c08ebae2eb19e7",
-            &mut expected_master_private_key as &mut [u8],
-        )
-        .unwrap();
-        assert_eq!(expected_master_private_key, m.secret_key()?.to_le_bytes());
+            let m = seed.to_master_key();
+            let mut expected_master_chain_code = [0u8; 32];
+            hex::decode_to_slice(&tv.master_chain_code, &mut expected_master_chain_code as &mut [u8]).unwrap();
+            assert_eq!(expected_master_chain_code, m.chain_code());
 
-        {
-            // m/0'
-            let ck = m.step(&Segment::from_u32(Segment::HARDEN_MASK))?;
+            let mut expected_master_private_key = [0u8; 32];
+            hex::decode_to_slice(&tv.master_private_key, &mut expected_master_private_key as &mut [u8]).unwrap();
+            assert_eq!(expected_master_private_key, m.secret_key()?.to_le_bytes());
 
-            let mut expected_chain_code = [0u8; 32];
-            hex::decode_to_slice(
-                &"47fdacbd0f1097043b78c63c20c34ef4ed9a111d980047ad16282c7ae6236141",
-                &mut expected_chain_code as &mut [u8],
-            )
-            .unwrap();
-            assert_eq!(expected_chain_code, ck.chain_code());
+            for c in tv.chains.iter() {
+                let ck = seed.derive(&c.chain)?;
 
-            let mut expected_private_key = [0u8; 32];
-            hex::decode_to_slice(
-                &"edb2e14f9ee77d26dd93b4ecede8d16ed408ce149b6cd80b0715a2d911a0afea",
-                &mut expected_private_key as &mut [u8],
-            )
-            .unwrap();
-            assert_eq!(expected_private_key, ck.secret_key()?.to_le_bytes());
+                let mut expected_chain_code = [0u8; 32];
+                hex::decode_to_slice(&c.chain_code, &mut expected_chain_code as &mut [u8]).unwrap();
+                assert_eq!(expected_chain_code, ck.chain_code());
+
+                let mut expected_private_key = [0u8; 32];
+                hex::decode_to_slice(&c.private_key, &mut expected_private_key as &mut [u8]).unwrap();
+                assert_eq!(expected_private_key, ck.secret_key()?.to_le_bytes());
+            }
         }
 
         Ok(())
