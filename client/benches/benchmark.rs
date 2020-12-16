cd557d0938d7f15bf2f62262b72e18193cf0e07e
tensor-programming <tensordeveloper@gmail.com>
2020-12-15 22:31:25 -0500

    add lisence.

--- client/benches/benchmark.rs
+++ client/benches/benchmark.rs
@@ -1,3 +1,6 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use iota_stronghold::{RecordHint, Stronghold};
