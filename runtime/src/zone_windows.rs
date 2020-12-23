57a9997f265dac55d6ebe3df1ad00032ff848e26
tensor-programming <tensordeveloper@gmail.com>
2020-12-23 13:27:24 -0500

    cleanup zone warnings

--- runtime/src/zone_windows.rs
+++ runtime/src/zone_windows.rs
@@ -2,18 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #[derive(PartialEq, Debug)]
-pub enum Error {
-}
+pub enum Error {}
 
-struct ZoneSpec {
-}
+struct ZoneSpec {}
 
 impl Default for ZoneSpec {
     fn default() -> Self {
-        Self { }
+        Self {}
     }
 }
 
+#[allow(dead_code)]
 impl ZoneSpec {
     pub fn run<F, T>(&self, f: F) -> crate::Result<T>
     where
