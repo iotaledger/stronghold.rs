edbdb49bcc312605d563a609aeff680828bfe3d0
tensor-programming <tensordeveloper@gmail.com>
2021-01-04 20:29:20 -0500

    cleanup bench and fuzz.

--- engine/snapshot/fuzz/src/compression.rs
+++ engine/snapshot/fuzz/src/compression.rs
@@ -1,9 +1,9 @@
 #![no_main]
 
+/// Requires Linux, MacOS or WSL to compile.  Use Cargo fuzz and the nightly toolchain.
 use libfuzzer_sys::fuzz_target;
 use snapshot::{compress, decompress};
 
-/// Requires Linux, MacOS or WSL to compile.
 fuzz_target!(|data: &[u8]| {
     let compressed = compress(data);
     let decompressed = decompress(&compressed).expect("failed to decompress the data");
