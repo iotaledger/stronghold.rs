05b1ef6ef1088d1c73ed4c292ef27f18a615be10
tensor-programming <tensordeveloper@gmail.com>
2021-01-08 18:05:48 -0500

    fmt me baby.

--- client/src/actors/client.rs
+++ client/src/actors/client.rs
@@ -2,8 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    actors::SMsg,
-    actors::{InternalMsg, InternalResults},
+    actors::{InternalMsg, InternalResults, SMsg},
     client::{Client, ClientMsg, ReadWrite},
     line_error,
     utils::{hd, ClientId, ResultMessage, StatusMessage},
