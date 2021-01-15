1a161ce76fb147ff7ae9d2ae6002f8be19e87180
elenaf9 <elena.frank@protonmail.com>
2021-01-15 18:08:44 +0100

    feat(clent): handle CommunicationEvent in client actor

--- communication/examples/actor.rs
+++ communication/examples/actor.rs
@@ -22,10 +22,7 @@ use core::time::Duration;
 use libp2p::core::identity::Keypair;
 use riker::actors::*;
 use serde::{Deserialize, Serialize};
-use stronghold_communication::{
-    actor::{CommsActorConfig, CommunicationActor, CommunicationEvent},
-    behaviour::message::P2PReqResEvent,
-};
+use stronghold_communication::actor::{CommsActorConfig, CommunicationActor, CommunicationEvent};
 
 // The type of request that is send to/ from remote peers.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -40,56 +37,30 @@ pub enum Response {
 }
 
 // An independent actor that communicates with the `CommunicationActor` via the channel.
-struct TestActor {
-    chan: ChannelRef<CommunicationEvent<Request, Response>>,
-}
-
-impl ActorFactoryArgs<ChannelRef<CommunicationEvent<Request, Response>>> for TestActor {
-    fn create_args(chan: ChannelRef<CommunicationEvent<Request, Response>>) -> Self {
-        TestActor { chan }
-    }
-}
+#[derive(Default)]
+struct TestActor;
 
 impl Actor for TestActor {
     type Msg = CommunicationEvent<Request, Response>;
 
-    fn pre_start(&mut self, ctx: &Context<Self::Msg>) {
-        // subscribe to the topic where the CommunicationActor publishes its messages.
-        let topic = Topic::from("from_swarm");
-        let sub = Box::new(ctx.myself());
-        self.chan.tell(Subscribe { actor: sub, topic }, None);
-    }
-
     fn post_start(&mut self, ctx: &Context<Self::Msg>) {
-        self.chan.tell(
-            Publish {
-                msg: CommunicationEvent::GetSwarmInfo,
-                topic: Topic::from("to_swarm"),
-            },
-            Some(BasicActorRef::from(ctx.myself())),
-        );
+        let communication_actor = ctx.select("*").unwrap();
+        communication_actor.try_tell(CommunicationEvent::<Request, Response>::GetSwarmInfo, ctx.myself());
     }
 
-    fn recv(&mut self, ctx: &Context<Self::Msg>, msg: Self::Msg, _sender: Sender) {
+    fn recv(&mut self, ctx: &Context<Self::Msg>, msg: Self::Msg, sender: Sender) {
         println!("{}: -> got msg: {:?}", ctx.myself.name(), msg);
-        if let CommunicationEvent::Message(P2PReqResEvent::Req {
-            peer_id,
+        if let CommunicationEvent::Request {
+            peer_id: _,
             request_id: Some(request_id),
             request: Request::Ping,
-        }) = msg
+        } = msg
         {
-            let response = CommunicationEvent::Message(P2PReqResEvent::Res {
-                peer_id,
+            let response = CommunicationEvent::<Request, Response>::Response {
                 request_id,
-                response: Response::Pong,
-            });
-            self.chan.tell(
-                Publish {
-                    msg: response,
-                    topic: Topic::from("to_swarm"),
-                },
-                Some(BasicActorRef::from(ctx.myself())),
-            );
+                result: Ok(Response::Pong),
+            };
+            sender.unwrap().try_tell(response, ctx.myself()).unwrap();
         }
     }
 }
@@ -99,12 +70,10 @@ fn main() {
     let sys = ActorSystem::new().unwrap();
     // The key pair for encryption
     let local_keys = Keypair::generate_ed25519();
-    // configure the CommunicationActor to use the channel.
-    let chan: ChannelRef<CommunicationEvent<Request, Response>> = channel("p2p", &sys).unwrap();
-    let config = CommsActorConfig::new(local_keys, None, Some(chan.clone()), None);
+    let actor_ref = sys.actor_of::<TestActor>("test-actor").unwrap();
+    let config = CommsActorConfig::new(local_keys, None, BasicActorRef::from(actor_ref));
     sys.actor_of_args::<CommunicationActor<Request, Response>, _>("communication-actor", config)
         .unwrap();
-    sys.actor_of_args::<TestActor, _>("test-actor", chan).unwrap();
     // Run for 5 minutes
     std::thread::sleep(Duration::from_secs(300));
 }
