c88686a74f4a364d94499a58a45b7c007fe07dba
tensor-programming <tensordeveloper@gmail.com>
2021-02-26 14:35:01 -0500

    add basic lib docs.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -3,6 +3,16 @@
 
 #![no_std]
 
+//! Stronghold Protected-access Memory Runtime.
+//!
+//! These modules contain an interface for allocating and protecting
+//! the memory of secrets in Stronghold.  Data is protected from being accessed
+//! outside of a limited scope. Instead it must be accessed via the
+//! provided interfaces.
+//!
+//! Memory allocations are protected by guard pages before and after the
+//! allocation, an underflow canary, and are zeroed out when freed.
+
 mod boxed;
 mod guarded;
 mod guarded_vec;
