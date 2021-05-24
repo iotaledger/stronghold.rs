b595fbc0eff6a5df5968bd11908a6de253418800
Matthias Kandora <matthias.kandora@iota.org>
2021-05-24 09:55:26 +0200

    feat(example): add communication example

--- client/examples/cli/main.rs
+++ client/examples/cli/main.rs
@@ -5,11 +5,8 @@ mod arguments;
 
 use arguments::*;
 use clap::Clap;
-use futures::{executor::block_on, StreamExt};
-use iota_stronghold::{home_dir, naive_kdf, Location, RecordHint, ResultMessage, StatusMessage, Stronghold};
-
-#[cfg(feature = "communication")]
-use iota_stronghold::Multiaddr;
+use futures::executor::block_on;
+use iota_stronghold::{home_dir, naive_kdf, Location, RecordHint, StatusMessage, Stronghold};
 
 use riker::actors::*;
 use std::error::Error;
