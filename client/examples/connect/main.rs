c2e5671516492be6a74d0affc99305d7c428742a
Matthias Kandora <matthias.kandora@iota.org>
2021-06-15 13:08:53 +0200

    fix: refactor to hide feature gated code

--- client/examples/connect/main.rs
+++ client/examples/connect/main.rs
@@ -10,93 +10,98 @@
 //! cargo run --example connect --features communication -- --relay-id <base58 addr string> --relay-address "/ip4/0.0.0.0/tcp/7001"
 //! ```
 
-use clap::Clap;
-use futures::executor::block_on;
-use iota::{Multiaddr, RelayDirection, ResultMessage};
-use iota_stronghold as iota;
-use log::*;
-use std::{collections::HashMap, error::Error, str::FromStr};
+use std::error::Error;
 
 #[cfg(feature = "communication")]
-use iota::{ActorSystem, PeerId, Stronghold};
+mod comm {
+    use super::*;
 
-#[derive(Clap)]
-#[clap(name = "Client Connect", about = "Remote Stronghold connection example")]
-struct ConnectApp {
-    #[clap(
-        long,
-        short = 'l',
-        about = "Comma separated peer_id=multiaddr",
-        value_delimiter = ","
-    )]
-    peers: Option<String>,
+    pub use clap::Clap;
+    pub use futures::executor::block_on;
+    pub use iota::{ActorSystem, Multiaddr, PeerId, RelayDirection, ResultMessage, Stronghold};
+    pub use iota_stronghold as iota;
+    pub use log::*;
+    pub use std::{collections::HashMap, str::FromStr};
 
-    #[clap(long, short = 'p', about = "The peer id of a relay server")]
-    relay_id: PeerId,
+    #[derive(Clap)]
+    #[clap(name = "Client Connect", about = "Remote Stronghold connection example")]
+    pub struct ConnectApp {
+        #[clap(
+            long,
+            short = 'l',
+            about = "Comma separated peer_id=multiaddr",
+            value_delimiter = ","
+        )]
+        pub peers: Option<String>,
 
-    #[clap(long, short = 'r', about = "The multiaddress of the relay server")]
-    relay_address: Multiaddr,
-}
+        #[clap(long, short = 'p', about = "The peer id of a relay server")]
+        pub relay_id: PeerId,
 
-fn parse(input: String) -> Result<HashMap<PeerId, Multiaddr>, Box<dyn Error>> {
-    let mut result: HashMap<PeerId, Multiaddr> = HashMap::new();
-
-    for token in input.split(',') {
-        let entry = parse_entry(token.to_string())?;
-        result.insert(entry.0, entry.1);
+        #[clap(long, short = 'r', about = "The multiaddress of the relay server")]
+        pub relay_address: Multiaddr,
     }
 
-    Ok(result)
-}
+    pub fn parse(input: String) -> Result<HashMap<PeerId, Multiaddr>, Box<dyn Error>> {
+        let mut result: HashMap<PeerId, Multiaddr> = HashMap::new();
 
-/// Tries to parse an entry consisting of a pair of "peer_id=multiaddr" as string
-/// Returns a tuple of [`PeerId`] and [`Multiaddr`]
-fn parse_entry(input: String) -> Result<(PeerId, Multiaddr), Box<dyn Error>> {
-    let tokens: Vec<&str> = input.split('=').collect();
-
-    let parsed_peer_id = PeerId::from_str(tokens[0])?;
-    let parsed_multiaddr = Multiaddr::from_str(tokens[1])?;
-
-    Ok((parsed_peer_id, parsed_multiaddr))
-}
-
-/// Tries to connect to (optional) peers and adds a relay
-/// server in between.
-fn connect_to(
-    stronghold: &mut Stronghold,
-    relay_id: PeerId,
-    relay_address: Multiaddr,
-    peers: Option<HashMap<PeerId, Multiaddr>>,
-) -> Result<(), Box<dyn Error>> {
-    stronghold.spawn_communication();
-
-    block_on(async {
-        stronghold.get_swarm_info().await;
-
-        if let ResultMessage::Error(err) = stronghold
-            .add_peer(relay_id, Some(relay_address), Some(RelayDirection::Both))
-            .await
-        {
-            error!("Could not connect to relay. Cause :{}", err);
+        for token in input.split(',') {
+            let entry = parse_entry(token.to_string())?;
+            result.insert(entry.0, entry.1);
         }
-    });
 
-    if let Some(peers) = peers {
-        info!("Adding peers");
-
-        peers.into_iter().for_each(|(id, addr)| {
-            block_on(async {
-                stronghold.add_peer(id, Some(addr), None).await;
-            });
-        });
+        Ok(result)
     }
 
-    Ok(())
-}
+    /// Tries to parse an entry consisting of a pair of "peer_id=multiaddr" as string
+    /// Returns a tuple of [`PeerId`] and [`Multiaddr`]
+    pub fn parse_entry(input: String) -> Result<(PeerId, Multiaddr), Box<dyn Error>> {
+        let tokens: Vec<&str> = input.split('=').collect();
 
+        let parsed_peer_id = PeerId::from_str(tokens[0])?;
+        let parsed_multiaddr = Multiaddr::from_str(tokens[1])?;
+
+        Ok((parsed_peer_id, parsed_multiaddr))
+    }
+
+    /// Tries to connect to (optional) peers and adds a relay
+    /// server in between.
+    pub fn connect_to(
+        stronghold: &mut Stronghold,
+        relay_id: PeerId,
+        relay_address: Multiaddr,
+        peers: Option<HashMap<PeerId, Multiaddr>>,
+    ) -> Result<(), Box<dyn Error>> {
+        stronghold.spawn_communication();
+
+        block_on(async {
+            stronghold.get_swarm_info().await;
+
+            if let ResultMessage::Error(err) = stronghold
+                .add_peer(relay_id, Some(relay_address), Some(RelayDirection::Both))
+                .await
+            {
+                error!("Could not connect to relay. Cause :{}", err);
+            }
+        });
+
+        if let Some(peers) = peers {
+            info!("Adding peers");
+
+            peers.into_iter().for_each(|(id, addr)| {
+                block_on(async {
+                    stronghold.add_peer(id, Some(addr), None).await;
+                });
+            });
+        }
+
+        Ok(())
+    }
+}
 fn main() -> Result<(), Box<dyn Error>> {
     #[cfg(feature = "communication")]
     {
+        use comm::*;
+
         let app = ConnectApp::parse();
 
         let client_path = b"client_path";
@@ -124,6 +129,7 @@ mod tests {
     use super::*;
 
     #[test]
+    #[cfg(feature = "communication")]
     fn test_parse() {
         let test_string =
             "12D3KooWGAS2jVkydjJp4DPcjAKKteC1gMBkTaxdH8vrrpFZecLR=/ip4/0.0.0.0/tcp/9001,12D3KooWQkhxr6iaYuoRpiQ4b6fDKHC3YMerKpA4Rnmk7TND9b6B=/ip4/0.0.0.0/tcp/9001".to_string();
