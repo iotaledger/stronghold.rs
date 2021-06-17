d2ec34091fb7cccaa06c64e457783cea4b5d7a14
Daniel Thompson-Yvetot <denjell@mailscript.com>
2021-06-17 12:57:17 +0200

    chore(app): no need for devServer to open browser
    Signed-off-by: Daniel Thompson-Yvetot <denjell@mailscript.com>

--- products/desktop/quasar.conf.js
+++ products/desktop/quasar.conf.js
@@ -77,7 +77,7 @@ module.exports = function (/* ctx */) {
     devServer: {
       https: false,
       port: 8080,
-      open: true // opens browser window automatically
+      open: false // opens browser window automatically
     },
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
