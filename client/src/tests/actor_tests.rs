7037de3a7c8da3507b236efd8d501459f01ef408
Matthias Kandora <matthias.kandora@iota.org>
2021-08-20 12:02:56 +0200

    feat: factored out tests from registry

---
+++ client/src/tests/actor_tests.rs
@@ -0,0 +1,71 @@
+// Copyright 2020-2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::actors::{GetClient, InsertClient, Registry, RemoveClient};
+use actix::Actor;
+use engine::vault::ClientId;
+
+#[actix::test]
+async fn test_insert_client() {
+    let registry = Registry::default().start();
+
+    for d in 'a'..'z' {
+        let format_str = format!("{}", d).repeat(24);
+        let id_str = format_str.as_str().as_bytes();
+        let n = registry
+            .send(InsertClient {
+                id: ClientId::load(id_str).unwrap(),
+            })
+            .await;
+
+        assert!(n.is_ok());
+    }
+}
+
+#[actix::test]
+async fn test_get_client() {
+    let registry = Registry::default().start();
+
+    for d in 'a'..'z' {
+        let format_str = format!("{}", d).repeat(24);
+        let id_str = format_str.as_str().as_bytes();
+        assert!(registry
+            .send(InsertClient {
+                id: ClientId::load(id_str).unwrap(),
+            })
+            .await
+            .is_ok());
+    }
+
+    assert!(registry
+        .send(GetClient {
+            id: ClientId::load("b".repeat(24).as_bytes()).unwrap(),
+        })
+        .await
+        .is_ok());
+}
+
+#[actix::test]
+async fn test_remove_client() {
+    let registry = Registry::default().start();
+
+    for d in 'a'..'z' {
+        let format_str = format!("{}", d).repeat(24);
+        let id_str = format_str.as_str().as_bytes();
+        assert!(registry
+            .send(InsertClient {
+                id: ClientId::load(id_str).unwrap(),
+            })
+            .await
+            .is_ok());
+    }
+
+    if let Ok(result) = registry
+        .send(RemoveClient {
+            id: ClientId::load("a".repeat(24).as_bytes()).unwrap(),
+        })
+        .await
+    {
+        assert!(result.is_ok())
+    }
+}
