d6a45f2f54ed7ab4e2569e1c420440356c208491
felsweg-iota <felsweg-iota@users.noreply.github.com>
2021-08-26 11:54:05 +0000

    deploy: ad57181e7c5baa4b6ccb66fb464667c97967db08

--- rust/iota_stronghold/actors/secure/sidebar-items.js
+++ rust/iota_stronghold/actors/secure/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["SnapshotError",""],["VaultError",""]],"mod":[["messages","Message types for [`SecureClientActor`]"],["procedures",""]],"type":[["Store","Store typedef on `engine::store::Cache`"]]});
+initSidebarItems({"enum":[["SnapshotError",""],["StoreError",""],["VaultError",""]],"mod":[["messages","Message types for [`SecureClientActor`]"],["procedures",""]],"type":[["Store","Store typedef on `engine::store::Cache`"]]});
