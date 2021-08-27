3689e5b65fe6cc586351619e47b316bac464af27
Matthias Kandora <matthias.kandora@iota.org>
2021-08-27 15:05:41 +0200

    fix: remove remaining riker dependency

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,7 +1,5 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-mod ask;
 pub mod test_utils;
-pub use ask::ask;
 pub use stronghold_derive::{GuardDebug, RequestPermissions};
