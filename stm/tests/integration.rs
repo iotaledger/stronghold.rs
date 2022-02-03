8e2f084d71d690441f189cb24549920686afaf54
Matthias Kandora <matthias.kandora@iota.org>
2022-02-03 12:40:48 +0100

    feat: blocking future without channels

--- stm/tests/integration.rs
+++ stm/tests/integration.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::sync::Arc;
-use stronghold_stm::*;
+use stronghold_stm::{ctrl::FutureBlocker, transactional, TVar, Transaction};
 
 #[tokio::test]
 async fn test_single_transaction() {
@@ -37,4 +37,20 @@ async fn test_multiple_access() {
     let result = var.read_atomic();
     assert!(result.is_ok());
     assert_eq!(result.expect("Failed to unwrap result"), 33);
+}
+
+#[tokio::test]
+async fn test_blocking_future() {
+    let expected = 1024usize;
+
+    let blocker = FutureBlocker::new(async move { Ok(expected) });
+    let b1 = blocker.clone();
+
+    let r1 = tokio::spawn(blocker);
+    let r2 = tokio::spawn(async move { b1.wake().await });
+
+    let actual = r1.await.expect("Unable to join task").expect("Transaction failure");
+
+    assert_eq!(actual, expected);
+    r2.await.expect("");
 }
