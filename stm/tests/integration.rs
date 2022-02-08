2d1c520329b747259a7196e7de74f689bba221b2
Matthias Kandora <matthias.kandora@iota.org>
2022-02-08 09:07:04 +0100

    fix: remove explicit typing of transaction

--- stm/tests/integration.rs
+++ stm/tests/integration.rs
@@ -2,8 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::sync::Arc;
-use stronghold_stm::{transactional, TVar, Transaction};
+use stronghold_stm::{transactional, TVar};
 
+#[cfg(test)]
+#[ctor::ctor]
+/// This function will be run before any of the tests
 fn init_logger() {
     let _ = env_logger::builder()
         .is_test(true)
@@ -18,12 +21,10 @@ mod simple {
 
     #[tokio::test]
     async fn test_single_transaction() {
-        init_logger();
-
         let var: TVar<usize> = TVar::new(21);
         let v2 = var.clone();
 
-        assert!(transactional(move |tx: &Transaction<usize>| {
+        assert!(transactional(move |tx| {
             let a = tx.read(&v2).unwrap();
             tx.write(*a + 42, &v2).unwrap();
             Ok(())
@@ -36,18 +37,16 @@ mod simple {
 
     #[tokio::test]
     async fn test_multiple_transactions() {
-        init_logger();
-
         let var: TVar<usize> = TVar::new(21);
         let v2 = var.clone();
         let v3 = var.clone();
 
-        let r2 = tokio::spawn(transactional(move |tx: &Transaction<usize>| {
+        let r2 = tokio::spawn(transactional(move |tx| {
             tx.write(42, &v3).unwrap();
             Ok(())
         }));
 
-        let r1 = tokio::spawn(transactional(move |tx: &Transaction<usize>| {
+        let r1 = tokio::spawn(transactional(move |tx| {
             let a = tx.read(&v2).unwrap();
             tx.write(*a + 42, &v2).unwrap();
             Ok(())
@@ -65,14 +64,15 @@ mod simple {
 
     #[tokio::test]
     async fn test_multiple_access() {
-        init_logger();
-
         let var: TVar<usize> = TVar::new(33);
 
         let result = var.read();
         assert!(result.is_ok());
         assert_eq!(result.expect("Failed to unwrap result"), Arc::new(33));
     }
+
+    #[tokio::test]
+    async fn test_multiple_types() {}
 }
 
 #[cfg(test)]
