2e62eaa2fac336449f82c4d3478182075fcd8478
Alexandre Dang <alexandre.dang@iota.org>
2022-02-22 18:24:15 +0100

    Add refresh in non contiguous memory

--- engine/new_runtime/src/memories/noncontiguous_memory.rs
+++ engine/new_runtime/src/memories/noncontiguous_memory.rs
@@ -1,6 +1,6 @@
 use crate::memories::buffer::Buffer;
 use crate::memories::file_memory::FileMemory;
-use crate::crypto_utils::crypto_box::{BoxProvider, Key};
+use crate::crypto_utils::crypto_box::{BoxProvider};
 use crate::locked_memory::ProtectedConfiguration::*;
 use crate::locked_memory::ProtectedMemory;
 use crate::locked_memory::LockedConfiguration::{self, *};
@@ -73,14 +73,10 @@ impl<P: BoxProvider> LockedMemory<u8, P> for NonContiguousMemory<P>
     }
 
     /// Unlocks the memory and returns an unlocked Buffer
+    // To retrieve secret value you xor the hash contained in shard1 with value in shard2
     fn unlock(&self, config: LockedConfiguration<P>) -> Result<Buffer<u8>, MemoryError> {
-
         let mut data1 = [0u8; NC_DATA_SIZE];
-        if let RamShard(buf1) = &self.shard1 {
-            sha::SHA256(&buf1.borrow(), &mut data1);
-        } else {
-            panic!("{}", IMPOSSIBLE_CASE);
-        }
+        sha::SHA256(&self.get_buffer_from_shard1().borrow(), &mut data1);
 
         let data = match &self.shard2 {
             RamShard(b2) => {
@@ -98,8 +94,10 @@ impl<P: BoxProvider> LockedMemory<u8, P> for NonContiguousMemory<P>
     }
 }
 
+
 impl<P: BoxProvider> NonContiguousMemory<P>
 {
+    // Check that the given configurations are correct for allocation
     fn check_config(config: &LockedConfiguration<P>) -> Result<(), MemoryError> {
         match config {
             NCRamAndFileConfig(Some(size)) => {
@@ -119,6 +117,64 @@ impl<P: BoxProvider> NonContiguousMemory<P>
             _ => Err(ConfigurationNotAllowed)
         }
     }
+
+    fn get_buffer_from_shard1(&self) -> &Buffer<u8> {
+        match &self.shard1 {
+            RamShard(buf) => buf,
+            _ => panic!("{}", IMPOSSIBLE_CASE),
+        }
+    }
+
+
+    // Refresh the shards to increase security, may be called every _n_ seconds or
+    // punctually
+    fn refresh(self) -> Result<Self, MemoryError> {
+        NonContiguousMemory::check_config(&self.config)?;
+        let random = P::random_vec(NC_DATA_SIZE).expect("Failed to generate random vec");
+        let mut hash_of_random = [0u8; NC_DATA_SIZE];
+
+        // Refresh shard1
+        let data_of_old_shard1: &[u8] = &self.get_buffer_from_shard1().borrow();
+        let new_data1 = xor(data_of_old_shard1, &random);
+        let new_shard1 = RamShard(Buffer::alloc(&new_data1, BufferConfig(NC_DATA_SIZE))?);
+
+        let new_shard2;
+        let new_config;
+        let mut hash_of_old_shard1 = [0u8; NC_DATA_SIZE];
+        let mut hash_of_new_shard1 = [0u8; NC_DATA_SIZE];
+        sha::SHA256(&data_of_old_shard1, &mut hash_of_old_shard1);
+        sha::SHA256(&new_data1, &mut hash_of_new_shard1);
+
+        match &self.shard2 {
+            RamShard(b2) => {
+                let new_data2 = xor(&b2.borrow(), &hash_of_old_shard1);
+                let new_data2 = xor(&new_data2, &hash_of_new_shard1);
+                new_shard2 = RamShard(Buffer::alloc(&new_data2, BufferConfig(NC_DATA_SIZE))?);
+                new_config = NCRamConfig(Some(NC_DATA_SIZE));
+            },
+            FileShard(fm) => {
+                let buf = fm.unlock(FileConfig(None))
+                    .expect("Failed to unlock file memory");
+                let new_data2 = xor(&buf.borrow(), &hash_of_old_shard1);
+                let new_data2 = xor(&new_data2, &hash_of_new_shard1);
+                let new_fm = FileMemory::alloc(&new_data2,
+                                               FileConfig(Some(NC_DATA_SIZE)))?;
+                new_shard2 = FileShard(new_fm);
+                // new_shard2 = FileShard(fm.lock(new_data2, FileConfig(Some(NC_DATA_SIZE)))?);
+                new_config = NCRamAndFileConfig(Some(NC_DATA_SIZE));
+            },
+            _ => panic!("{}", IMPOSSIBLE_CASE)
+
+        };
+
+        Ok(
+            NonContiguousMemory {
+                shard1: new_shard1,
+                shard2: new_shard2,
+                config: new_config
+            }
+        )
+    }
 }
 
 fn xor(a: &[u8], b: &[u8]) -> [u8; NC_DATA_SIZE] {
@@ -135,6 +191,8 @@ impl<P: BoxProvider> Debug for NonContiguousMemory<P> {
         write!(f, "hidden")
     }
 }
+
+
 
 //##### Zeroize
 impl<T: Zeroize + Bytes, P: BoxProvider> Zeroize for MemoryShard<T, P> {
@@ -216,6 +274,45 @@ mod tests {
         assert!(buf.is_ok());
         let buf = buf.unwrap();
         assert_eq!((&*buf.borrow()), &data);
+    }
+
+    #[test]
+    fn test_refresh() {
+        let data = Provider::random_vec(NC_DATA_SIZE).unwrap();
+        let ncm = NonContiguousMemory::<Provider>::alloc(&data, NCRamAndFileConfig(Some(NC_DATA_SIZE)));
+        assert!(ncm.is_ok());
+        let ncm = ncm.unwrap();
+
+        let shard1_before_refresh = ncm.get_buffer_from_shard1().clone();
+        let shard2_before_refresh =
+            if let FileShard(fm) = &ncm.shard2 {
+                fm.unlock(FileConfig(None)).unwrap()
+            } else {
+                panic!("{}", IMPOSSIBLE_CASE)
+            };
+
+        let ncm = ncm.refresh();
+        assert!(ncm.is_ok());
+        let ncm = ncm.unwrap();
+
+        let shard1_after_refresh = ncm.get_buffer_from_shard1();
+        let shard2_after_refresh =
+            if let FileShard(fm) = &ncm.shard2 {
+                fm.unlock(FileConfig(None)).unwrap()
+            } else {
+                panic!("{}", IMPOSSIBLE_CASE)
+            };
+
+
+        // Check that secrets is still ok after refresh
+        let buf = ncm.unlock(NCRamConfig(None));
+        assert!(buf.is_ok());
+        let buf = buf.unwrap();
+        assert_eq!((&*buf.borrow()), &data);
+
+        // Check that refresh change the shards
+        assert_ne!(&*shard1_before_refresh.borrow(), &*shard1_after_refresh.borrow());
+        assert_ne!(&*shard2_before_refresh.borrow(), &*shard2_after_refresh.borrow());
     }
 
     #[test]
