420a7024bb715d41c500ef8a142f18b75d766091
Matthias Kandora <matthias.kandora@iota.org>
2022-02-23 11:04:08 +0100

    feat: removed old code; add non-blocking queue / stack for write scaling

--- stm/src/lib.rs
+++ stm/src/lib.rs
@@ -8,39 +8,33 @@
 
 /// TODO: This mod should be replaced by the upcoming memory features in the runtime!
 pub mod boxedalloc;
-pub mod ctrl;
-pub mod errors;
+pub mod nonblock;
 pub mod rlu;
-pub mod transaction;
-pub mod types;
 
-pub use errors::TransactionError;
-use transaction::Strategy;
-pub use transaction::Transaction;
-pub use types::{TLog, TVar};
+pub use rlu::{RLUVar, RluContext, TransactionError, RLU};
 
 /// TODO: this should be replaced by the upcoming memory features in the runtime!
 pub use boxedalloc::LockedMemory;
 
-/// This creates an asynchronous operation that runs atomically inside a transaction. Shared
-/// memory must be passed as [`TVar`] to read from and write to it. The transaction is retried
-/// until it succeeds. As of now, this could hang the execution if certain edge cases are being hit:
-/// - interleaving reads and writes, blocking each other.
-pub async fn transactional<T, F>(program: F) -> Result<(), TransactionError>
-where
-    T: Send + Sync + LockedMemory,
-    F: Fn(&Transaction<T>) -> Result<(), TransactionError> + Send + 'static,
-{
-    Transaction::with_strategy(program, Strategy::Retry).await
-}
+// This creates an asynchronous operation that runs atomically inside a transaction. Shared
+// memory must be passed as [`TVar`] to read from and write to it. The transaction is retried
+// until it succeeds. As of now, this could hang the execution if certain edge cases are being hit:
+// - interleaving reads and writes, blocking each other.
+// pub async fn transactional<T, F>(program: F) -> Result<(), TransactionError>
+// where
+//     T: Send + Sync + LockedMemory,
+//     F: Fn(&Transaction<T>) -> Result<(), TransactionError> + Send + 'static,
+// {
+//     Transaction::with_strategy(program, Strategy::Retry).await
+// }
 
-/// This creates an asynchronous operation that runs atomically inside a transaction. Shared
-/// memory must be passed as [`TVar`] to read from and write to it. The transaction is aborted
-/// if the commit to shared memory fails
-pub async fn single<T, F>(program: F) -> Result<(), TransactionError>
-where
-    T: Send + Sync + LockedMemory,
-    F: Fn(&Transaction<T>) -> Result<(), TransactionError> + Send + 'static,
-{
-    Transaction::with_strategy(program, Strategy::Abort).await
-}
+// This creates an asynchronous operation that runs atomically inside a transaction. Shared
+// memory must be passed as [`TVar`] to read from and write to it. The transaction is aborted
+// if the commit to shared memory fails
+// pub async fn single<T, F>(program: F) -> Result<(), TransactionError>
+// where
+//     T: Send + Sync + LockedMemory,
+//     F: Fn(&Transaction<T>) -> Result<(), TransactionError> + Send + 'static,
+// {
+//     Transaction::with_strategy(program, Strategy::Abort).await
+// }
