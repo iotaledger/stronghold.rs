0542aa84d85e8c43b6885dc025c9b198575325bc
Matthias Kandora <matthias.kandora@iota.org>
2022-03-01 18:41:51 +0100

    feat: add cache with rlu

--- client_new/src/lib.rs
+++ client_new/src/lib.rs
@@ -11,8 +11,8 @@ pub use stronghold_std::*;
 #[cfg(feature = "std")]
 mod stronghold_std {
 
-    use rlu::{RLUVar, Read, RluContext, Write, RLU};
-    use std::{error::Error, path::Path, sync::Arc};
+    use rlu::{RLUObject, RLUVar, Read, RluContext, Write, RLU};
+    use std::{collections::HashMap, error::Error, hash::Hash, path::Path, sync::Arc};
 
     pub type Result<T> = core::result::Result<T, Box<dyn Error>>;
 
@@ -81,6 +81,111 @@ mod stronghold_std {
 
         pub async fn execute_procedure() -> Result<()> {
             todo!()
+        }
+    }
+
+    // type integration
+
+    /// Prototypical (LRU) cache impl
+    pub struct Cache<K, V>
+    where
+        K: Eq + Clone,
+        V: Clone,
+    {
+        inner: RLUObject<HashMap<K, RLUObject<V>>>,
+    }
+
+    impl<K, V> Clone for Cache<K, V>
+    where
+        K: Eq + Hash + Clone,
+        V: Clone,
+    {
+        fn clone(&self) -> Self {
+            Self {
+                inner: self.inner.clone(),
+            }
+        }
+    }
+
+    impl<K, V> Default for Cache<K, V>
+    where
+        K: Eq + Hash + Clone,
+        V: Clone,
+    {
+        fn default() -> Self {
+            Self {
+                inner: HashMap::new().into(),
+            }
+        }
+    }
+
+    impl<K, V> Cache<K, V>
+    where
+        K: Eq + Hash + Clone,
+        V: Clone,
+    {
+        pub fn insert(&self, key: K, value: V) -> Result<()> {
+            let tvar = self.inner.var();
+
+            Ok(self.inner.ctrl().execute(move |mut ctx| {
+                let key = key.clone();
+                let value = value.clone();
+
+                let mut var = ctx.get_mut(&tvar)?;
+                (*var).insert(key, value.into());
+                Ok(())
+            })?)
+        }
+
+        pub fn get(&self, key: &K) -> Option<Arc<RLUVar<V>>> {
+            let tvar = self.inner.var();
+            let map = tvar.get();
+
+            if let Some(inner) = map.get(key) {
+                return Some(inner.var());
+            }
+
+            None
+        }
+
+        pub fn modify(&self, key: &K, value: V) -> Result<()> {
+            let tvar = self.inner.var();
+            let map = tvar.get();
+
+            if let Some(inner) = map.get(key) {
+                let inner_tvar = inner.var();
+                inner.ctrl().execute(|mut ctx| {
+                    let mut guard = ctx.get_mut(&inner_tvar)?;
+                    *guard = value.clone();
+
+                    Ok(())
+                })?;
+            }
+            Ok(())
+        }
+
+        pub fn delete(&self, key: &K) -> Result<()> {
+            let ctrl = self.inner.ctrl();
+            let var = self.inner.var();
+
+            Ok(ctrl.execute(|mut ctx| {
+                let mut guard = ctx.get_mut(&var)?;
+                (*guard).remove(key);
+
+                Ok(())
+            })?)
+        }
+
+        pub fn delete_all(&self) -> Result<()> {
+            let ctrl = self.inner.ctrl();
+            let var = self.inner.var();
+
+            Ok(ctrl.execute(|mut ctx| {
+                let mut guard = ctx.get_mut(&var)?;
+                (*guard).clear();
+
+                Ok(())
+            })?)
         }
     }
 }
--- client_new/tests/integration.rs
+++ client_new/tests/integration.rs
@@ -1,2 +1,22 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
+
+#[cfg(feature = "std")]
+mod stronghold_test_std {
+
+    use iota_stronghold_new::{Cache, Result};
+
+    #[test]
+    fn test_cache_concurrent() -> Result<()> {
+        let cache = Cache::default();
+        cache.insert(1223usize, "hello, world")?;
+
+        assert!(cache.get(&1223).is_some());
+        assert_eq!(*cache.get(&1223).unwrap().get(), "hello, world");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_cache_async() {}
+}
