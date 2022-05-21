0783dc7a87ddaff60b73b3c95a16fe75301b2c40
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-21 10:04:35 +0200

    Fix branch name

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 path: path.resolve(__dirname, 'docs'),
                 routeBasePath: 'stronghold.rs',
                 sidebarPath: path.resolve(__dirname, 'sidebars.js'),
-                editUrl: 'https://github.com/iotaledger/stronghold/edit/dev/documentation',
+                editUrl: 'https://github.com/iotaledger/stronghold/edit/dev-refactor/documentation',
                 remarkPlugins: [require('remark-code-import'), require('remark-import-partial'), require('remark-remove-comments')],
             }
         ],
