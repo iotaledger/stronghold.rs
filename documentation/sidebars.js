8379ca0a5cde53274efd468e89ccb42b4c9dacc2
Lucas Tortora <lucas.tortora@iota.org>
2022-06-28 12:30:52 -0300

    ADded raw loader Added welcome page Added CLI how tos Updated getting started section

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -10,120 +10,147 @@
  */
 
 module.exports = {
-  mySidebar: [{
-    type: 'doc',
-    label: 'Getting Started',
-    id: 'getting_started',
-  },
-  {
-    type: "category",
-    label: "Key Concepts",
-    items: [
-      {
+    mySidebar: [{
         type: 'doc',
-        id: 'key_concepts/retrospective',
-        label: 'Retrospective'
-      },]
-  },
-  {
-    type: 'category',
-    label: 'Reference',
-    items: [
-      {
-        type:'doc',
-        id: 'reference/overview',
-        label: "Overview"
-      },{
-      type: 'category',
-      label: 'Structure',
-      items: [{
+        label: 'Welcome',
+        id: 'welcome',
+    }, {
         type: 'doc',
-        id: 'reference/structure/overview',
-        label: 'Overview'
-      }, {
-        type: 'doc',
-        id: 'reference/structure/client',
-        label: 'Client'
-      },
-      {
-        type: 'category',
-        label: 'Engine',
-        items: [
-          {
-            type: 'doc',
-            id: 'reference/structure/engine/overview',
-            label: 'Overview'
-          },
-
-          {
-            type: 'doc',
-            id: 'reference/structure/engine/snapshot',
-            label: 'Snapshot'
-          },
-
-          {
-            type: 'doc',
-            id: 'reference/structure/engine/vault',
-            label: 'Vault'
-          },
-
-          {
-            type: 'doc',
-            id: 'reference/structure/engine/store',
-            label: 'Store'
-          },
-
-          {
-            type: 'doc',
-            id: 'reference/structure/engine/runtime',
-            label: 'Runtime'
-          },
-        ],
-      },
-      {
-        type: 'doc',
-        id: 'reference/structure/p2p',
-        label: 'P2P Communication'
-      },
-      {
-        type: 'doc',
-        id: 'reference/structure/derive',
-        label: 'Derive'
-      },
-      {
-        type: 'doc',
-        id: 'reference/structure/utils',
-        label: 'Utils'
-      },
-      ]
+        label: 'Getting Started',
+        id: 'getting_started',
     },
-    {
-      type: 'category',
-      label: 'Specification',
-      items: [{
-        type: 'doc',
-        id: 'reference/specs/overview',
-        label: 'Overview'
-      }, {
-        type: 'doc',
-        id: 'reference/specs/scope',
-        label: 'Scope'
-      }, {
-        type: 'doc',
-        id: 'reference/specs/engineering',
-        label: 'Engineering'
-      },]
-    },]
-  },
-  {
-    type: 'doc',
-    id: 'contribute',
-    label: 'Contribute'
-  },
-  {
-    type: 'link',
-    href: 'https://github.com/iotaledger/stronghold.rs',
-    label: 'GitHub'
-  },
-  ]
+        {
+
+            type: "category",
+            label: "How Tos",
+            items: [
+                {
+                    type: "category",
+                    label: "Command Line Interface (CLI)",
+                    items:
+                        [
+                            'how_tos/cli/running_examples',
+                            'how_tos/cli/generate_key_pair',
+                            'how_tos/cli/store_read_write',
+                            'how_tos/cli/generate_bip39',
+                            'how_tos/cli/generate_slip_10',
+                            'how_tos/cli/derive_slip_10',
+                            'how_tos/cli/create_snapshot',
+                            'how_tos/cli/read_snapshot',
+                            'how_tos/cli/recover_seed_with_mnemonic',
+                        ]
+                },
+            ]
+        },
+        {
+            type: "category",
+            label: "Explanations",
+            items: [
+                {
+                    type: 'doc',
+                    id: 'key_concepts/retrospective',
+                    label: 'Retrospective'
+                },]
+        },
+        {
+            type: 'category',
+            label: 'Reference',
+            items: [
+                {
+                    type: 'doc',
+                    id: 'reference/overview',
+                    label: "Overview"
+                }, {
+                    type: 'category',
+                    label: 'Structure',
+                    items: [{
+                        type: 'doc',
+                        id: 'reference/structure/overview',
+                        label: 'Overview'
+                    }, {
+                        type: 'doc',
+                        id: 'reference/structure/client',
+                        label: 'Client'
+                    },
+                        {
+                            type: 'category',
+                            label: 'Engine',
+                            items: [
+                                {
+                                    type: 'doc',
+                                    id: 'reference/structure/engine/overview',
+                                    label: 'Overview'
+                                },
+
+                                {
+                                    type: 'doc',
+                                    id: 'reference/structure/engine/snapshot',
+                                    label: 'Snapshot'
+                                },
+
+                                {
+                                    type: 'doc',
+                                    id: 'reference/structure/engine/vault',
+                                    label: 'Vault'
+                                },
+
+                                {
+                                    type: 'doc',
+                                    id: 'reference/structure/engine/store',
+                                    label: 'Store'
+                                },
+
+                                {
+                                    type: 'doc',
+                                    id: 'reference/structure/engine/runtime',
+                                    label: 'Runtime'
+                                },
+                            ],
+                        },
+                        {
+                            type: 'doc',
+                            id: 'reference/structure/p2p',
+                            label: 'P2P Communication'
+                        },
+                        {
+                            type: 'doc',
+                            id: 'reference/structure/derive',
+                            label: 'Derive'
+                        },
+                        {
+                            type: 'doc',
+                            id: 'reference/structure/utils',
+                            label: 'Utils'
+                        },
+                    ]
+                },
+                {
+                    type: 'category',
+                    label: 'Specification',
+                    items: [{
+                        type: 'doc',
+                        id: 'reference/specs/overview',
+                        label: 'Overview'
+                    }, {
+                        type: 'doc',
+                        id: 'reference/specs/scope',
+                        label: 'Scope'
+                    }, {
+                        type: 'doc',
+                        id: 'reference/specs/engineering',
+                        label: 'Engineering'
+                    },]
+                },]
+        },
+        {
+            type: 'doc',
+            id: 'contribute',
+            label: 'Contribute'
+        },
+        {
+            type: 'link',
+            href: 'https://github.com/iotaledger/stronghold.rs',
+            label: 'GitHub'
+        },
+    ]
 };
