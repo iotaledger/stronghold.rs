f7ce6eeecefaafcd055237aa1a2be5609c983498
Alexandre Dang <alexandre.dang@iota.org>
2022-10-14 11:11:41 +0200

    Add documentation for deadlock analysis

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 module.exports = {
     plugins: [
@@ -10,9 +12,19 @@ module.exports = {
                 routeBasePath: 'stronghold.rs',
                 sidebarPath: path.resolve(__dirname, 'sidebars.js'),
                 editUrl: 'https://github.com/iotaledger/stronghold/edit/dev/documentation',
-                remarkPlugins: [require('remark-code-import'), require('remark-import-partial'), require('remark-remove-comments')],
+                remarkPlugins: [require('remark-code-import'), require('remark-import-partial'), require('remark-remove-comments'), math],
+                rehypePlugins: [katex],
             }
         ],
     ],
     staticDirectories: [path.resolve(__dirname, 'static')],
+    stylesheets: [
+        {
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+            type: 'text/css',
+            integrity:
+            'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+            crossorigin: 'anonymous',
+        },
+    ],
 };
