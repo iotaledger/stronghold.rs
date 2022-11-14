eb27efc97106754d77f6acee18aeaf952384f989
samuel_rufi <samuel.rufinatscha@iota.org>
2022-11-14 17:01:55 +0100

    Disable frag module for android and ios targets

--- engine/runtime/src/memories/mod.rs
+++ engine/runtime/src/memories/mod.rs
@@ -3,6 +3,7 @@
 
 pub mod buffer;
 pub mod file_memory;
+#[cfg(not(any(target_os="android", target_os="ios")))]
 pub mod frag;
 pub mod noncontiguous_memory;
 pub mod ram_memory;
--- engine/runtime/src/memories/noncontiguous_memory.rs
+++ engine/runtime/src/memories/noncontiguous_memory.rs
@@ -9,13 +9,15 @@ use crate::{
     memories::{
         buffer::Buffer,
         file_memory::FileMemory,
-        frag::{Frag, FragStrategy},
         ram_memory::RamMemory,
     },
     utils::*,
     MemoryError::*,
     *,
 };
+#[cfg(not(any(target_os="android", target_os="ios")))]
+use crate::memories::frag::{Frag, FragStrategy};
+
 use core::{
     fmt::{self, Debug, Formatter},
     marker::PhantomData,
@@ -47,6 +49,7 @@ pub enum NCConfig {
     FullFile,
     FullRam,
     RamAndFile,
+    #[cfg(not(any(target_os="android", target_os="ios")))]
     FragAllocation(FragStrategy),
 }
 use NCConfig::*;
@@ -57,6 +60,7 @@ use NCConfig::*;
 enum MemoryShard {
     File(FileMemory),
     Ram(RamMemory),
+    #[cfg(not(any(target_os="android", target_os="ios")))]
     Frag(Frag<[u8; NC_DATA_SIZE]>),
 }
 use MemoryShard::*;
@@ -203,6 +207,7 @@ impl MemoryShard {
                 Ok((File(fmem1), File(fmem2)))
             }
 
+            #[cfg(not(any(target_os="android", target_os="ios")))]
             FragAllocation(strat) => {
                 let (frag1, frag2) = Frag::alloc_initialized(
                     *strat,
@@ -226,6 +231,7 @@ impl MemoryShard {
                 let v = buf.borrow().to_vec();
                 Ok(v)
             }
+            #[cfg(not(any(target_os="android", target_os="ios")))]
             Frag(frag) => {
                 if frag.is_live() {
                     Ok(frag.get()?.to_vec())
@@ -249,6 +255,7 @@ impl Zeroize for MemoryShard {
         match self {
             File(fm) => fm.zeroize(),
             Ram(buf) => buf.zeroize(),
+            #[cfg(not(any(target_os="android", target_os="ios")))]
             Frag(frag) => frag.zeroize(),
         }
     }
