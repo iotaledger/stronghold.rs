cda735ce5cd37dd769e7d8e2f7e56ff19f37e689
Thibault Martinez <thibault@iota.org>
2023-03-02 15:50:24 +0100

    Split into module files

---
+++ engine/src/snapshot/migration/mod.rs
@@ -0,0 +1,82 @@
+mod error;
+mod v2;
+mod v3;
+
+use std::{
+    fs::{File, OpenOptions},
+    io::{Read, Write},
+    path::Path,
+};
+
+use crypto::{
+    ciphers::{chacha::XChaCha20Poly1305, traits::Aead},
+    hashes::{blake2b, Digest},
+    keys::{age, x25519},
+};
+
+// These dependencies must not change between versions,
+// otherwise migration will work differently.
+use self::error::Error;
+use crate::snapshot::{compress, decompress};
+
+pub enum Version<'a> {
+    V2 {
+        path: &'a Path,
+        key: &'a [u8; 32],
+        aad: &'a [u8],
+    },
+    V3 {
+        path: &'a Path,
+        password: &'a [u8],
+    },
+}
+
+impl<'a> Version<'a> {
+    pub fn v2(path: &'a Path, key: &'a [u8; 32], aad: &'a [u8]) -> Self {
+        Self::V2 { path, key, aad }
+    }
+
+    pub fn v3(path: &'a Path, password: &'a [u8]) -> Self {
+        Self::V3 { path, password }
+    }
+}
+
+/// Magic bytes (bytes 0-4 in a snapshot file) aka PARTI
+const MAGIC: [u8; 5] = [0x50, 0x41, 0x52, 0x54, 0x49];
+
+#[inline]
+fn guard<E>(cond: bool, err: E) -> Result<(), E> {
+    if cond {
+        Ok(())
+    } else {
+        Err(err)
+    }
+}
+
+fn migrate_from_v2_to_v3(
+    v2_path: &Path,
+    v2_key: &[u8; 32],
+    v2_aad: &[u8],
+    v3_path: &Path,
+    v3_pwd: &[u8],
+) -> Result<(), Error> {
+    let v = v2::read_snapshot(v2_path, v2_key, v2_aad)?;
+    v3::write_snapshot(&v[..], v3_path, v3_pwd, &[])
+}
+
+pub fn migrate(prev: Version, next: Version) -> Result<(), Error> {
+    match (prev, next) {
+        (
+            Version::V2 {
+                path: v2_path,
+                key: v2_key,
+                aad: v2_aad,
+            },
+            Version::V3 {
+                path: v3_path,
+                password: v3_pwd,
+            },
+        ) => migrate_from_v2_to_v3(v2_path, v2_key, v2_aad, v3_path, v3_pwd),
+        _ => Err(Error::BadMigrationVersion),
+    }
+}
---
+++ engine/src/snapshot/migration/v3.rs
@@ -0,0 +1,47 @@
+use super::*;
+
+/// Key size for the ephemeral key
+const KEY_SIZE: usize = 32;
+/// Key type alias.
+pub type Key = [u8; KEY_SIZE];
+
+const VERSION_V3: [u8; 2] = [0x3, 0x0];
+
+pub fn read<I: Read>(input: &mut I, password: &Key, _associated_data: &[u8]) -> Result<Vec<u8>, Error> {
+    let mut age = Vec::new();
+    input.read_to_end(&mut age)?;
+    age::decrypt_vec(password, &age[..]).map_err(|e| Error::AgeError(e))
+}
+
+pub fn write<O: Write>(plain: &[u8], output: &mut O, password: &Key, _associated_data: &[u8]) -> Result<(), Error> {
+    let age = age::encrypt_vec(password, plain);
+    output.write_all(&age[..])?;
+    Ok(())
+}
+
+/// Atomically encrypt, add magic and version bytes as file-header, and [`write`][self::write] the specified
+/// plaintext to the specified path.
+///
+/// This is achieved by creating a temporary file in the same directory as the specified path (same
+/// filename with a salted suffix). This is currently known to be problematic if the path is a
+/// symlink and/or if the target path resides in a directory without user write permission.
+pub fn write_snapshot(plain: &[u8], path: &Path, password: &[u8], aad: &[u8]) -> Result<(), Error> {
+    guard(aad.is_empty(), Error::AadNotSupported)?;
+    let compressed_plain = compress(plain);
+
+    // emulate constructor `KeyProvider::with_passphrase_hashed` with `D = blake2b`
+    let mut password_hash = [0_u8; KEY_SIZE];
+    let mut h = crypto::hashes::blake2b::Blake2b256::default();
+    h.update(password);
+    h.finalize_into((&mut password_hash).into());
+
+    let mut f = OpenOptions::new().write(true).create_new(true).open(path)?;
+    // write magic and version bytes
+    f.write_all(&MAGIC)?;
+    f.write_all(&VERSION_V3)?;
+    // blake2b hash of password is used as encryption password in age
+    write(&compressed_plain, &mut f, &password_hash, aad)?;
+    f.sync_all()?;
+
+    Ok(())
+}
